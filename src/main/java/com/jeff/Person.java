349a9c7fa73b87bbf51891c9b4cc116f2a0a5bec
Justnice <chiojhe@gmail.com>
2020-11-04 14:29:29 +0800

    Initial commit

---
+++ src/main/java/com/jeff/Person.java
@@ -0,0 +1,7 @@
+package com.jeff;
+
+public class Person {
+    public void hello(){
+        System.out.println("helloworld");
+    }
+}
---
+++ src/main/kotlin/com/jeff/Hello.kt
@@ -0,0 +1,16 @@
+package com.jeff
+
+fun main() {
+//    println("HelloWorld")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+
+
+}
+
+class Human {
+    fun hello() {
+        println("HelloWorld")
+    }
+}
